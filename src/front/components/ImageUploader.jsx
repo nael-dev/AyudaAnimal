30137e06cf1381b7d913cc803348dbfd3cbb6abe
Nael <naeldiaz@proton.me>
2025-09-27 17:44:35 +0000

    fallo cloudinary

--- src/front/components/ImageUploader.jsx
+++ src/front/components/ImageUploader.jsx
@@ -4,16 +4,18 @@ import { auto } from '@cloudinary/url-gen/actions/resize';
 import { autoGravity } from '@cloudinary/url-gen/qualifiers/gravity';
 import { AdvancedImage } from '@cloudinary/react';
 
-const ImageUploader = ({idImage, size=400}) => {
-  const cld = new Cloudinary({ cloud: { cloudName: 'dul3zl1av' } });
-  
-  const img = cld
-        .image(idImage)
-        .format('auto') 
-        .quality('auto')
-        .resize(auto().gravity(autoGravity()).width(size).height(size)); 
+const ImageUploader = ({ idImage, size = 400 }) => {
+  if (!idImage) return null; // <--- evita errores si no hay imagen
 
-  return (<AdvancedImage className={'rounded-3'} cldImg={img}/>);
+  const cld = new Cloudinary({ cloud: { cloudName: 'dul3zl1av' } });
+
+  const img = cld
+    .image(idImage)
+    .format('auto')
+    .quality('auto')
+    .resize(auto().gravity(autoGravity()).width(size).height(size));
+
+  return <AdvancedImage className="rounded-3" cldImg={img} />;
 };
 
-export default ImageUploader
+export default ImageUploader;
