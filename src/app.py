165d4834a9584d2ebe6b2d7b50b607753eb9389a
Nael <naeldiaz@proton.me>
2025-09-27 15:49:17 +0000

    cambio bases de datos

--- src/app.py
+++ src/app.py
@@ -27,7 +27,7 @@ if db_url is not None:
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
-
+    
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db, compare_type=True)
 db.init_app(app)
